1ae57ab38104a4a0c3aafc8687117512b166c167
Radoslaw Grzanka <r.grzanka@ks-software.com>
2019-01-31 11:09:33 +0100

    added Emojify support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,10 +18,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-emojify-mode t)
  '(org-agenda-files (quote ("~/org/praca.org" "~/org/osobiste.org")))
  '(package-selected-packages
    (quote
-    (auto-package-update csharp-mode yasnippet-snippets counsel nyan-mode gruvbox-theme neotree nord-theme nord expand-region hungry-delete popup-kill-ring symon dmenu diminish spaceline spacemacs-theme highlight-symbol jedi flycheck auto-complete magit powershell undo-tree swiper ace-window org zenburn-theme yasnippet which-key use-package try org-bullets helm beacon)))
+    (emojify auto-package-update csharp-mode yasnippet-snippets counsel nyan-mode gruvbox-theme neotree nord-theme nord expand-region hungry-delete popup-kill-ring symon dmenu diminish spaceline spacemacs-theme highlight-symbol jedi flycheck auto-complete magit powershell undo-tree swiper ace-window org zenburn-theme yasnippet which-key use-package try org-bullets helm beacon)))
  '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
