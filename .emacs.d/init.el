c22798169b7ef067e773a9dab9d9e2bd8dbba867
Radoslaw Grzanka <r.grzanka@ks-software.com>
2019-11-25 22:13:09 +0100

    Usunięcie bezsensownych ustawień z init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,6 @@
  '(custom-safe-themes
    (quote
     ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(org-agenda-files (quote ("~/org/praca.org" "~/org/osobiste.org")))
  '(package-selected-packages
    (quote
     (abl-mode virtualenvwrapper company-quickhelp material-theme blacken py-autopep8 python-mode company-jedi rainbow-delimiters rainbow-delimeters multiple-cursors projectile omnisharp ox-hugo emojify auto-package-update csharp-mode yasnippet-snippets counsel nyan-mode gruvbox-theme neotree nord-theme nord expand-region hungry-delete popup-kill-ring symon dmenu diminish spaceline spacemacs-theme highlight-symbol jedi flycheck auto-complete magit powershell undo-tree swiper ace-window org zenburn-theme yasnippet which-key use-package try org-bullets helm beacon)))
